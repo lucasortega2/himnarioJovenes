6402953c6fe1c3dc50967d0f5f7532e89737032b
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-09-10 18:09:14 -0300

    output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
-  output: 'server',
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel(),
 });
