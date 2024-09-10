a9f49fd1a15c87e6ee41fe23a49c3df94008b506
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-09-10 18:07:29 -0300

    adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db()],
+  output: 'server',
+  adapter: vercel()
 });
