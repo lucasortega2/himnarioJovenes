dfabbdc31b4ea7a2fbe9a14645f9e8176f6d2f67
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-09-11 13:02:13 -0300

    himno to Himno db

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,35 @@
-import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
-import { db, User, Session } from 'astro:db';
 import { Lucia } from 'lucia';
+import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
+import { db, Session, User } from 'astro:db';
 
 const adapter = new DrizzleSQLiteAdapter(
   db as any,
-  User as any,
   Session as any,
+  User as any,
 ); // your adapter
 
-export const lucia = new Lucia(adapter, {
+const lucia = new Lucia(adapter, {
   sessionCookie: {
     attributes: {
-      // set to `true` when using HTTPS
       secure: import.meta.env.PROD,
     },
   },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username,
+    };
+  },
 });
+
+export default lucia; // Exportación por defecto
 
 declare module 'lucia' {
   interface Register {
     Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
   }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
 }
