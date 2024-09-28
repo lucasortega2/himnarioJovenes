6bc049d2bc839a34d33611aca19a60a4be287421
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-09-28 15:21:15 -0300

    passing the all dbs to gethymns

--- src/pages/api/getHymns.ts
+++ src/pages/api/getHymns.ts
@@ -1,20 +1,27 @@
 import type { APIContext, APIRoute } from 'astro';
-import { db, Himnos } from 'astro:db';
+import { db, Himnario, Himnos, Jovenes, Suplementario } from 'astro:db';
 
 export const GET: APIRoute = async (context: APIContext) => {
   try {
     const hymns = await db.select().from(Himnos);
-    return new Response(JSON.stringify(hymns), {
-      status: 200,
-      headers: {
-        'content-Type': 'application/json',
+    const himnario = await db.select().from(Himnario);
+    const jovenes = await db.select().from(Jovenes);
+    const suplementario = await db.select().from(Suplementario);
+
+    return new Response(
+      JSON.stringify({ hymns, himnario, jovenes, suplementario }),
+      {
+        status: 200,
+        headers: {
+          'content-Type': 'application/json',
+        },
       },
-    });
+    );
   } catch (error) {
     return new Response(JSON.stringify({ error: 'Error al obtener himnos' }), {
       status: 500,
       headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
       },
     });
   }
