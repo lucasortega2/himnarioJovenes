4eac46e25ecdd52392c16789ea668a12e5aa39dc
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-10-10 19:18:10 -0300

    remove api key from gethymns

--- src/pages/api/getHymns.ts
+++ src/pages/api/getHymns.ts
@@ -2,14 +2,6 @@ import type { APIContext, APIRoute } from 'astro';
 import { db, Himnario, Himnos, Jovenes, Suplementario } from 'astro:db';
 
 export const GET: APIRoute = async (context: APIContext) => {
-  const apiKey = context.request.headers.get('API_KEY');
-
-  if (apiKey !== import.meta.env.API_KEY) {
-    return new Response(JSON.stringify({ message: 'Unauthorized' }), {
-      status: 401,
-    });
-  }
-
   try {
     const hymns = await db.select().from(Himnos);
     const himnario = await db.select().from(Himnario);
